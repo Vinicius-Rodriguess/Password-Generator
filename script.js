de2e2d6801fe89eb95311000f5577a091fe17468
Tk 000407 <rodrigues.vini.2004@gmail.com>
2023-06-26 09:59:00 -0300

    Remoção alert

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let buttunElement = document.querySelector('#button');
 let sizePassword = document.querySelector('#valor');
 let passWord = document.querySelector('#password');
 let containerPassword = document.querySelector('#container-password');
+let alerta = document.querySelector('.tooltip');
 
 let charsert = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#"
 let novaSenha = '';
@@ -18,15 +19,14 @@ function generatePassword(){
     let pass = "";
     for(let i = 0, n = charsert.length; i < sliderElement.value; ++i){
         pass += charsert.charAt(Math.floor(Math.random() * n));
-        pass
     }
-     
     containerPassword.classList.remove("hide");
     passWord.innerHTML = pass;
     novaSenha = pass;
+    alerta.innerHTML = "Clique na senha para copiar. 👆";
 }
 
 function copyPassword(){
-    alert("Senha copiada com sucesso!");
+    alerta.innerHTML = "Senha copiada com sucesso! 👍";
     navigator.clipboard.writeText(novaSenha);
 }
