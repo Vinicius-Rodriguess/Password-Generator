93e8855e22f691c7fe270ee72323342747bd3de0
Tk 000407 <rodrigues.vini.2004@gmail.com>
2023-10-30 08:48:45 -0300

    v2.3

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,9 @@
-let sliderElement = document.querySelector('#slider');
-let buttunElement = document.querySelector('#button');
-let sizePassword = document.querySelector('#valor');
-let passWord = document.querySelector('#password');
-let containerPassword = document.querySelector('#container-password');
-let alerta = document.querySelector('.tooltip');
+const sliderElement = document.querySelector('#slider');
+const buttunElement = document.querySelector('#button');
+const sizePassword = document.querySelector('#valor');
+const passWord = document.querySelector('#password');
+const containerPassword = document.querySelector('#container-password');
+const alerta = document.querySelector('.tooltip');
 
 const chkSimbolos = document.querySelector('.chk-simbolos');
 const chkMaiusculas = document.querySelector('.chk-maiusculas');
@@ -12,30 +12,31 @@ const chkNumeros = document.querySelector('.chk-numeros');
 const simbolos = ',.;~^[]{}!@#$%*()_+=-';
 
 const rand = (min, max) => Math.floor(Math.random() * (max - min) + min);
-const geraMaiuscula = () => String.fromCharCode(rand(65,91));
+const geraMaiuscula = () => String.fromCharCode(rand(65, 91));
 const geraMinuscula = () => String.fromCharCode(rand(97, 123));
 const geraNumero = () => String.fromCharCode(rand(48, 58));
 const geraSimbolo = () => simbolos[rand(0, simbolos.length)];
 
-function ChecaQuantidade(maiusculas, minusculas, numeros, simbolos) {
+function checaInput(maiusculas, minusculas, numeros, simbolos) {
     const alertError = document.querySelector('#error');
-    
-    if(maiusculas === false && minusculas === false && numeros === false && simbolos === false) {
+
+    if (!maiusculas && !minusculas && !numeros && !simbolos) {
         alertError.classList.remove('hide');
         containerPassword.classList.add("hide");
         return false;
-    } else {
-        alertError.classList.add('hide');
     }
+
+    alertError.classList.add('hide');
+
 }
 
 function geraSenha(quantidade, maiusculas, minusculas, numeros, simbolos) {
     const senhaArray = [];
     quantidade = Number(quantidade);
 
-    if(ChecaQuantidade(maiusculas, minusculas, numeros, simbolos) == false) return;
+    if (checaInput(maiusculas, minusculas, numeros, simbolos) == false) return;
 
-    for(let i = 0; i < quantidade; i++) {
+    for (let i = 0; i < quantidade; i++) {
         maiusculas && senhaArray.push(geraMaiuscula());
         minusculas && senhaArray.push(geraMinuscula());
         numeros && senhaArray.push(geraNumero());
@@ -52,21 +53,21 @@ function geraSenha(quantidade, maiusculas, minusculas, numeros, simbolos) {
 
 buttunElement.addEventListener('click', () => {
     geraSenha(
-      sliderElement.value,
-      chkMaiusculas.checked,
-      chkMinusculas.checked,
-      chkNumeros.checked,
-      chkSimbolos.checked
+        sliderElement.value,
+        chkMaiusculas.checked,
+        chkMinusculas.checked,
+        chkNumeros.checked,
+        chkSimbolos.checked
     )
 });
 
 sizePassword.innerHTML = sliderElement.value;
 
-sliderElement.oninput = function(){
+sliderElement.oninput = function () {
     sizePassword.innerHTML = this.value;
 }
 
-function copyPassword(){
+function copyPassword() {
     alerta.innerHTML = "Senha copiada com sucesso! 👍";
     navigator.clipboard.writeText(novaSenha);
 }
